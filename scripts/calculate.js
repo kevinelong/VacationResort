28302a866ed377ca26c75c7343afbc7427de7404
kevinelong <kevinelong@gmail.com>
2022-10-07 09:01:09 -0700

    added js input variable and button click

--- scripts/calculate.js
+++ scripts/calculate.js
@@ -6,7 +6,24 @@ function init(){
 window.onload = init;// wait until body content/html loaded into the window.
 */
 
-document.addEventListener("DOMContentLoaded", ()=>{ //BEGIN ANONYMOUS
+document.addEventListener("DOMContentLoaded", () => { //BEGIN ANONYMOUS
     //INIT CODE GOES HERE
-    
+    document.getElementById("calculate").addEventListener("click", () => {
+
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const checkinDate = document.getElementById("checkinDate").value;
+        const nights = document.getElementById("nights").value;
+        const adults = document.getElementById("adults").value;
+        const children = document.getElementById("children").value;
+
+        const queen = document.getElementById("queen").checked;
+        const king = document.getElementById("king").checked;
+        const suite = document.getElementById("suite").checked;
+
+        const none = document.getElementById("none").checked;
+        const senior = document.getElementById("senior").checked;
+        const military = document.getElementById("military").checked;
+
+    });
 });
