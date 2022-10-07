d57d217c7fb139f14b96ab9ceb36cca8e41a9608
kevinelong <kevinelong@gmail.com>
2022-10-07 08:01:54 -0700

    init

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+/*
+// OLD SCHOOL INIT - works only if its the only listener to the onload event
+function init(){
+    //all deferred code here
+}
+window.onload = init;// wait until body content/html loaded into the window.
+*/
+
+document.addEventListener("DOMContentLoaded", ()=>{ //BEGIN ANONYMOUS
+    //INIT CODE GOES HERE
+    
+});
